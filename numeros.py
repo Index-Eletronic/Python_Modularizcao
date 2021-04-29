81ecdc5ced68bbe1f141da2140595647051ec5ea
Fil Tuon <index.eletronic@gmail.com>
2021-04-29 19:34:44 -0300

    .

--- numeros.py
+++ numeros.py
@@ -1,6 +1,7 @@
-import uteis
+from uteis import numeros
+
 
 num = int(input('Digite um numero: '))
-fat = uteis.fatorial(num) # Importar def fatorial do Modulo uteis.py
+fat = numeros.fatorial(num) # Importar def fatorial do Modulo uteis.py
 print(f'O Fatorial de {num} é {fat}')
-print(f'O dobro de um {num} é {uteis.dobro(num)}')
+print(f'O dobro de um {num} é {numeros.dobro(num)}')
