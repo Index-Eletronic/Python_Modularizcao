e1160095e114d5d22fb27a67019b446b8cdbce13
Fil Tuon <index.eletronic@gmail.com>
2021-04-30 10:17:29 -0300

    .

--- exerc_modulo_moeda/teste.py
+++ exerc_modulo_moeda/teste.py
@@ -1,10 +1,13 @@
-def aumentar(preco, taxa):
+'''
+Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade(). Faça também um programa que importe esse módulo e use algumas dessas funções.
+'''
+
+import moeda
+# se fosse um pacote seria: from exerc_modulo_moeda import moeda
 
 
-def diminuir(preco, taxa):
-
-
-def dobro(preco):
-
-
-def aumentar(preco):
+p = float(input('Digite o preço R$: '))
+print(f'A Metade de {p} é {moeda.metade(p)}')
+print(f'O dobro de {p} é {moeda.dobro(p)}')
+print(f'Aumentando 10% temos R$: {moeda.aumentar(p, 10)}')
+print(f'Diminuindo 10% temos R$: {moeda.diminuir(p, 10)}')
