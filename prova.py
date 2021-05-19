4e5d00049867f0b48a6b891ebf9da661691a0793
Fil Tuon <index.eletronic@gmail.com>
2021-05-19 20:46:42 -0300

    .

---
+++ prova.py
@@ -0,0 +1,20 @@
+num = [2, 8, 4, 7]
+num.pop()
+num.insert(1, 3)
+num.append(6)
+print(num)
+
+letras = ('J', 'X', 'M', 'O', 'A', 'K') #Tupla
+print(letras.index('A'))
+
+val = list(range(1, 5))
+print(val)
+
+num = [3, 6, 4, 1]
+for n1, n2 in enumerate(num):
+    print(n1 + n2)
+
+num2 = [8, 2, 4, 2, 5, 2]
+print(num.count(2))
+print(num.count(2))
+print(num2.tot(2))
